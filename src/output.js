cd8a1e081b2ece139d86b8f4400ad29a940a637e
SendaRaven <r.sahlmann@gmx.de>
2019-07-17 12:37:36 +0200

    added function and output for time; some beautifying

--- src/output.js
+++ src/output.js
@@ -1,13 +1,23 @@
 function outputCurrent(content) {
-    console.log(content.name);
     const outputField = document.querySelector("h1");
     outputField.innerText = `${content.name} ${(content.main.temp - 273.15).toFixed(2)}°C`;
     console.log(content);
+    const dateOutput = document.querySelector("#date")
+    dateOutput.innerText = time();
+
 }
 
 function outputForecast(content) {
     console.log(content);
-    
+
 }
+
+function time() {
+    let currentDate = new Date()
+    let daysOfWeek = ["Sunday", "Monday", "Tuesday","Wednesday", "Thursday", "Friday", "Saturday" ]
+    return `${daysOfWeek[currentDate.getDay()]} ${currentDate.getHours()}:${(currentDate.getMinutes()<10?'0':'') + currentDate.getMinutes() }`;
+}
+
+
 
 export { outputCurrent, outputForecast }
