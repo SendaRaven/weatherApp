87cf31026d5ad74d98ded26a6e69212993e3bb1d
SendaRaven <r.sahlmann@gmx.de>
2019-07-18 12:27:59 +0200

    fixed output for forcaste data

--- src/output.js
+++ src/output.js
@@ -3,8 +3,9 @@ import { weatherUnitConverter as converter } from "./weatherUnitConverter.js";
 function outputCurrent(content) {
     console.log(content);
     const outputField = document.querySelector("h1");
-    outputField.innerText = `${content.name} ${converter(content.main.temp)}`;
-
+    outputField.innerText = `${content.name},${content.sys.country} ${converter(content.main.temp)}`;
+    console.log(content.main.temp);
+    
     const weatherData = document.querySelector("#weatherData");
     weatherData.innerText = `Wind: ${content.wind.speed} m/s
     Humidity: ${content.main.humidity}%
@@ -23,18 +24,30 @@ function outputForecast(content) {
     console.log(content);
     let i = 0;
     let substrDate = Number(content.list[0].dt_txt.slice(8, 10));
-
+    let minArray = [];
+    let maxArray = [];
     while (i <= 4) {
         //let resultArray = [];
         let result = content.list.filter(date => date.dt_txt.slice(8, 10) == substrDate);
         let mapedResult = result.map(data => data.main.temp);
-        console.log(i, Math.min(...mapedResult), i, Math.max(...mapedResult))
+        minArray.push(Math.min(...mapedResult));
+        maxArray.push(Math.max(...mapedResult));
         //console.log(result);
         substrDate++;
         i++;
-        // result = [];
-    }
 
+    }     
+        //console.log(minArray);
+        //console.log(maxArray);
+        let minRow = document.getElementsByClassName("min");
+        let maxRow = document.getElementsByClassName("max");
+        //console.log(minRow);
+        
+        for (let i = 1; i < 5; i++) {
+            //console.log(minRow[0])
+            minRow[i].innerText = converter(minArray[i]);
+            maxRow[i].innerText = converter(maxArray[i]);
+        }
 }
 
 function time() {
